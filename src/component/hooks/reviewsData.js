4c10fc87d49d1a7439dc6a342435e33bf746ab2d
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-24 05:19:49 +0100

    added data to reviewData file

--- src/component/hooks/reviewsData.js
+++ src/component/hooks/reviewsData.js
@@ -1,32 +1,32 @@
-const reviewsData = [
+export default reviewsData = [
   {
     id: 1,
-    name: '<NAME>',
-    review: 'This is a great place to stay',
+    name: '<1>',
+    review: '1 This is a great place to stay',
     rating: 5,
   },
   {
     id: 2,
-    name: '<NAME>',
-    review: 'This is a great place to stay',
+    name: '<2>',
+    review: '2 This is a great place to stay',
     rating: 4,
   },
   {
     id: 3,
-    name: '<NAME>',
-    review: 'This is a great place to stay',
+    name: '<3>',
+    review: '3 This is a great place to stay',
     rating: 3,
   },
   {
     id: 4,
-    name: '<NAME>',
-    review: 'This is a great place to stay',
+    name: '<4>',
+    review: '4 This is a great place to stay',
     rating: 2,
   },
   {
     id: 5,
-    name: '<NAME>',
-    review: 'This is a great place to stay',
+    name: '<5>',
+    review: '5 This is a great place to stay',
     rating: 1,
   },
 ]
